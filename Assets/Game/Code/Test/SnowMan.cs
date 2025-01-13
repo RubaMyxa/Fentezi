86ecf0d8900171c6d7bdaffad992fef4d1cfb901
RubaMyxa <glebshum10@gmail.com>
2025-01-13 20:31:58 +0100

    Code SnowMan

--- Assets/Game/Code/Test/SnowMan.cs
+++ Assets/Game/Code/Test/SnowMan.cs
@@ -4,6 +4,14 @@ namespace Assets.Game.Code.Test
 {
     public class SnowMan : MonoBehaviour
     {
-        
+        public void Print()
+        {
+            print(transform.position);
+        }
+
+        public void SnowManDestroy()
+        {
+            Destroy(gameObject);
+        }
     }
 }
--- Assets/Game/Code/Test/TestPlayer.cs
+++ Assets/Game/Code/Test/TestPlayer.cs
@@ -4,6 +4,14 @@ namespace Assets.Game.Code.Test
 {
     public class TestPlayer : MonoBehaviour
     {
-        
+
+        private void OnTriggerEnter2D(Collider2D collision)
+        {
+            if (collision.CompareTag("SnowMan"))
+            {
+                collision.GetComponent<SnowMan>().Print();
+                collision.GetComponent<SnowMan>().SnowManDestroy();
+            }
+        }
     }
 }
