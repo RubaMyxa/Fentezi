e7358ffbfa0bf072bafbd0fb0ec3f812386e4bc7
RubaMyxa <glebshum10@gmail.com>
2025-01-15 20:17:04 +0100

    SnowMan

--- Assets/Game/Code/Test/SnowMan.cs
+++ Assets/Game/Code/Test/SnowMan.cs
@@ -1,9 +1,15 @@
+using System;
 using UnityEngine;
 
 namespace Assets.Game.Code.Test
 {
     public class SnowMan : MonoBehaviour
     {
+        [SerializeField]
+        private Transform snowManSpawn;
+        [SerializeField]
+        private GameObject snowmanPrefab;
+
         public void Print()
         {
             print(transform.position);
@@ -12,6 +18,26 @@ namespace Assets.Game.Code.Test
         public void SnowManDestroy()
         {
             Destroy(gameObject);
+        }
+
+        public void Teleport(Vector2 target)
+        {
+            transform.position = target;
+        }
+
+        public void Scale(Vector2 target)
+        {
+            transform.localScale = target;
+        }
+
+        public void SnowManDestroyTrigger()
+        {
+            Destroy(GetComponent<BoxCollider2D>());
+        }
+
+        public void Spawn()
+        {
+            Instantiate(snowmanPrefab, snowManSpawn.position, Quaternion.identity);
         }
     }
 }
--- Assets/Game/Code/Test/TestPlayer.cs
+++ Assets/Game/Code/Test/TestPlayer.cs
@@ -4,14 +4,26 @@ namespace Assets.Game.Code.Test
 {
     public class TestPlayer : MonoBehaviour
     {
+        private Vector2 targetPosition;
+
+        private void Awake()
+        {
+            targetPosition = transform.position;
+        }
 
         private void OnTriggerEnter2D(Collider2D collision)
         {
             if (collision.CompareTag("SnowMan"))
             {
+                collision.GetComponent<SnowMan>().Teleport(targetPosition);
                 collision.GetComponent<SnowMan>().Print();
-                collision.GetComponent<SnowMan>().SnowManDestroy();
+                collision.GetComponent<SnowMan>().Scale(new Vector2(1.5f, 1.5f));
+                collision.GetComponent<SnowMan>().SnowManDestroyTrigger();
+                collision.GetComponent<SnowMan>().Spawn();
+                //collision.GetComponent<SnowMan>().SnowManDestroy();
             }
+
+
         }
     }
 }
